b3a3677c0fa03a95906d3f1ddfa3a2550c0dac2e
Kiran Patel <kiranpatel051@gmail.com>
2019-11-24 23:46:12 +0000

    unbxdAnalytics Events

--- hybris/bin/custom/unbxdanalytics/acceleratoraddon/web/src/com/unbxd/analytics/interceptors/beforeview/HybrisAnalyticsBeforeViewHandler.java
+++ hybris/bin/custom/unbxdanalytics/acceleratoraddon/web/src/com/unbxd/analytics/interceptors/beforeview/HybrisAnalyticsBeforeViewHandler.java
@@ -10,10 +10,14 @@
  */
 package com.unbxd.analytics.interceptors.beforeview;
 
+import com.unbxd.analytics.constants.UnbxdAnalyticsConstants;
 import de.hybris.platform.acceleratorservices.config.SiteConfigService;
 import de.hybris.platform.acceleratorservices.storefront.data.JavaScriptVariableData;
 import de.hybris.platform.addonsupport.config.javascript.BeforeViewJsPropsHandlerAdaptee;
 import de.hybris.platform.addonsupport.config.javascript.JavaScriptVariableDataFactory;
+import de.hybris.platform.catalog.model.CatalogModel;
+import de.hybris.platform.catalog.model.CatalogVersionModel;
+import de.hybris.platform.servicelayer.session.SessionService;
 import de.hybris.platform.servicelayer.session.impl.DefaultSessionTokenService;
 import de.hybris.platform.store.services.BaseStoreService;
 
@@ -35,6 +39,10 @@ public class HybrisAnalyticsBeforeViewHandler extends BeforeViewJsPropsHandlerAd
 	private static final String PIWIK_TRACKER_SITE_ID = "piwik.tracker.siteid.";
 	private static final String PIWIK_TRACKER_DEFAULT_SITE_ID = "piwik.tracker.siteid.default";
 	private static final String PIWIK_SITE_ID = "piwikSiteId";
+	private static final String UNBXD_ANALYTICS_CATALOG = "unbxdCatalog";
+	private static final String UNBXD_ANALYTICS_SITE_KEY = "unbxd.analytics.sitekey.";
+	private static final String UNBXD_ANALYTICS_DEFAULT_SITE_KEY = "unbxd.analytics.sitekey.default";
+	private static final String UNBXD_ANALYTICS_SITEKEY = "unbxdAnalyticsSiteKey";
 
 	@Resource(name = "siteConfigService")
 	private SiteConfigService siteConfigService;
@@ -44,6 +52,9 @@ public class HybrisAnalyticsBeforeViewHandler extends BeforeViewJsPropsHandlerAd
 
 	@Resource(name = "sessionTokenService")
 	private DefaultSessionTokenService sessionTokenService;
+
+	@Resource(name = "sessionService")
+	private SessionService sessionService;
 
 	@Override
 	public String beforeViewJsProps(final HttpServletRequest request, final HttpServletResponse response, final ModelMap model,
@@ -89,6 +100,10 @@ public class HybrisAnalyticsBeforeViewHandler extends BeforeViewJsPropsHandlerAd
 			jsVariables.put(getMessageSource().getAddOnName(), jsVariablesList);
 			final String piwikSiteId = getCurrentPiwikSiteId();
 			model.addAttribute(PIWIK_SITE_ID, piwikSiteId);
+			final String unbxdSiteKey = getCurrentUnbxdSiteKey();
+			model.addAttribute(UNBXD_ANALYTICS_SITEKEY, unbxdSiteKey);
+			final CatalogModel unbxdCatalog = getCurrentUnbxdCatalog();
+			model.addAttribute(UNBXD_ANALYTICS_CATALOG, unbxdCatalog);
 			//Setting the endpoint url as model attribute rather base.js.properties so that it can be overriden from local.properties
 			model.addAttribute("PIWIK_TRACKER_ENDPOINT_URL", siteConfigService.getProperty(PIWIK_TRACKER_ENDPOINT_URL));
 			model.addAttribute("PIWIK_TRACKER_ENDPOINT_HTTPS_URL", siteConfigService.getProperty(PIWIK_TRACKER_ENDPOINT_HTTPS_URL));
@@ -110,6 +125,26 @@ public class HybrisAnalyticsBeforeViewHandler extends BeforeViewJsPropsHandlerAd
 			piwikSiteId = siteConfigService.getProperty(PIWIK_TRACKER_DEFAULT_SITE_ID);
 		}
 		return piwikSiteId;
+	}
+
+	protected String getCurrentUnbxdSiteKey()
+	{
+		String unbxdSiteKey = siteConfigService.getProperty(UNBXD_ANALYTICS_SITE_KEY + baseStoreService.getCurrentBaseStore().getUid());
+		if (unbxdSiteKey == null)
+		{
+			unbxdSiteKey = siteConfigService.getProperty(UNBXD_ANALYTICS_DEFAULT_SITE_KEY);
+		}
+		return unbxdSiteKey;
+	}
+
+	protected CatalogModel getCurrentUnbxdCatalog()
+	{
+		final CatalogVersionModel unbxdCatalogVersion  = (CatalogVersionModel) sessionService.getAttribute("currentCatalogVersion");
+		if (unbxdCatalogVersion != null)
+		{
+			return unbxdCatalogVersion.getCatalog();
+		}
+		return baseStoreService.getCurrentBaseStore().getCatalogs().get(0);
 	}
 
 
